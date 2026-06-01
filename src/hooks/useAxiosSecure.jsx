e7a0389e9b55b8d00f3e17999291333e7033cbdc
parvejtalukder <mail@parvejhusentalukder.com>
2026-06-01 20:26:35 +0600

    Update: SERVER URL

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from './useAuth';
 import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
-    baseURL: "https://firstgoserver.vercel.app:3000/"
+    baseURL: "https://first-go-server.onrender.com/"
 })
 
 const useAxiosSecure = () => {
